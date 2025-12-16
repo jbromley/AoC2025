11ad9eef4d56c7a2a0fdd07d07c8767c1f21ba4c
Jay Bromley <jay@olokun>
2025-12-16 01:14:55 -0800

    Simplified day 6 part 1 solution
    Preparing new approach to part 2.

--- src/day-06.hs
+++ src/day-06.hs
@@ -1,8 +1,6 @@
 module Main where
 
-import AdventUtils (textToInt)
-import Data.Text (Text)
-import qualified Data.Text as T
+import Data.List (transpose)
 import System.Environment (getArgs)
 import Text.Printf (printf)
 
@@ -12,56 +10,46 @@ import Text.Printf (printf)
 - Solution should be the type of your solution. Typically is an Int, but It can be other things, like a list of numbers
          or a list of characters
 -}
-type Input = ([([Int] -> Int)], [[Int]])
+type Input = ([[Int] -> Int], [[Int]])
 
 type Solution = Int
 
 -- | parser transforms a raw bytestring (from your ./input/day-X.input) to your Input type.
 --   this is intended to use attoparsec for such a transformation. You can use Prelude's
 --   String if it fit better for the problem
-parser :: String -> Input
-parser s =
-  let ls = reverse $ map T.words $ T.lines $ T.strip $ T.pack s
+parser1 :: String -> Input
+parser1 s =
+  let ls = reverse $ map words $ lines s
    in case ls of
-        (opList:rest) ->
-          let ops = map opToFn opList
-              nums = transpose $ map (map textToInt) rest
-           in (ops, nums)
+        (opList:rest) -> (map opToFn opList, transpose $ map (map read) rest)
         [] -> error "input does not contain valid lines for parser"
 
-opToFn :: Text -> ([Int] -> Int)
-opToFn opText
-  | op == "+" = sum
-  | op == "*" = product
-  | otherwise = error (printf "opToFn invalid operator '%s'" op)
-  where
-    op = T.unpack opText
-
-transpose :: [[a]] -> [[a]]
-transpose [] = []
-transpose xss
-  | any null xss = []
-  | otherwise =
-    let heads = [h | (h:_) <- xss] -- Collect heads of non-empty lists
-        tails = [t | (_:t) <- xss] -- Collect tails of non-empty lists
-     in heads : transpose tails
+opToFn :: String -> ([Int] -> Int)
+opToFn "+" = sum
+opToFn "*" = product
+opToFn op = error (printf "illegal op '%s'" op)
 
 -- | The function which calculates the solution for part one
-solve1 :: Input -> Solution
-solve1 = sum . uncurry (zipWith ($))
+solve :: Input -> Solution
+solve = sum . uncurry (zipWith ($))
 
 -- | The function which calculates the solution for part two
 solve2 :: Input -> Solution
 solve2 = error "Part 2 Not implemented"
 
+finish :: Char -> [String] -> Int
+finish '+' xs = sum (map read xs)
+finish '*' xs = product (map read xs)
+finish op _xs = error (printf "invalid op '%s'" op)
+
 main :: IO ()
 main = do
   [part, filepath] <- getArgs
-  input <- parser <$> readFile filepath
+  input <- parser1 <$> readFile filepath
   if read @Int part == 1
     then do
       putStr "Day 6 part 1: "
-      print $ solve1 input
+      print $ solve input
     else do
       putStr "Day 6 part 2: "
       print $ solve2 input
