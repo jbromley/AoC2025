f88918be48f0ba064ff3a862f30516efdd7cf9a0
J. Bromley <jay@appliedinvention.com>
2025-12-18 09:47:09 -0800

    Added clamp function to AdventUtils

--- src/AdventUtils.hs
+++ src/AdventUtils.hs
@@ -1,7 +1,16 @@
-module AdventUtils (listToPair, pairRangeToList, pairRangeToSet, textToInt, textRangeToPair) where
+module AdventUtils
+  ( listToPair
+  , pairRangeToList
+  , pairRangeToSet
+  , textToInt
+  , textRangeToPair
+  ) where
 
-import Data.Text (Text, splitOn, unpack)
 import Data.IntSet (IntSet, fromList)
+import Data.Text (Text, splitOn, unpack)
+
+clamp :: Int -> Int -> Int -> Int
+clamp n lo hi = max lo (min hi n)
 
 listToPair :: [a] -> (a, a)
 listToPair [x1, x2] = (x1, x2)
